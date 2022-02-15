7e1db1e569cb49db1c2d1745feef907199629fd4
v_guangliu <v_guangliu@jintengtech.com>
2022-02-15 17:37:21 +0800

    feat;element-plus locale zhCn

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,57 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from 'vue';
+
+const currentPage = ref(3);
+const pageSize = ref(100);
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div>woshi vite</div>
   <el-button>dasdasda</el-button>
+  <el-button type="primary">dasdasda</el-button>
+
+  <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
+  <el-row>
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+  </el-row>
+  <el-row>
+    <el-button plain>朴素按钮</el-button>
+    <el-button type="primary" plain>主要按钮</el-button>
+    <el-button type="success" plain>成功按钮</el-button>
+    <el-button type="info" plain>信息按钮</el-button>
+    <el-button type="warning" plain>警告按钮</el-button>
+    <el-button type="danger" plain>危险按钮</el-button>
+  </el-row>
+  <el-progress
+    :text-inside="true"
+    :stroke-width="26"
+    :percentage="70"
+  ></el-progress>
+
+  <el-pagination
+    background
+    v-model:current-page="currentPage"
+    :page-sizes="[100, 200, 300, 400]"
+    v-model:page-size="pageSize"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="400"
+  >
+  </el-pagination>
 </template>
 
 <style lang="scss">
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue';
-import './assets/element/index.scss'
+import './assets/element/index.scss';
 import ElementPlus from 'element-plus';
+// element 国际化
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import App from './App.vue';
 
 const app = createApp(App);
-app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
 app.mount('#app');
